5808d8369562f5eed352cfd1211918ee6b22d879
seiv324 <stefan.habringer@gmail.com>
2024-06-14 15:49:29 +0200

    Created Datasets for Clustering and Created Presentation

---
+++ src/pythonCode.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+import seaborn as sns
+import csv
+from scipy.stats import multivariate_normal
+# define data paths
+file1 = './../data/41467_2024_46346_MOESM4_ESM.csv'
+file2 = './../data/41467_2024_46346_MOESM7_ESM.csv'
